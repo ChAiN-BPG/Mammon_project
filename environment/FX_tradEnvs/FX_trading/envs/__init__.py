44440f95742515d5a182ba61afe11ffcfb0eb311
ChAiN-BPG <71115615+ChAiN-BPG@users.noreply.github.com>
2020-12-17 22:41:59 +0700

    add true env

--- environment/FX_tradEnvs/FX_trading/envs/__init__.py
+++ environment/FX_tradEnvs/FX_trading/envs/__init__.py
@@ -1,3 +1,4 @@
-from FX_trading.envs.FX_trading_v2 import ForexEnv
+from FX_trading.envs.FX_trading_v2 import ForexEnv_test3
 from FX_trading.envs.FX_trading_v1 import ForexEnv_test2
 from FX_trading.envs.FX_trading_v0 import ForexEnv_test
+from FX_trading.envs.FX_Trading import ForexEnv
