4dcdf904734a029d2d066cd4657b2b7333d967db
ChAiN-BPG <71115615+ChAiN-BPG@users.noreply.github.com>
2021-02-22 15:54:39 +0700

    fix scaler model

--- data/Create_Data/testdata.py
+++ data/Create_Data/testdata.py
@@ -10,44 +10,44 @@ from sklearn.preprocessing import MinMaxScaler
 import pickle
 import random
 
-data = pd.read_excel('data/dataset/XM_EURUSD-2013_H1.xlsx',header=None)
-# close_diff = data.iloc[1:,5] - data.iloc[:len(data),5]
-# close_a = np.array(data.iloc[1:,5])
-# close_b = np.array(data.iloc[:len(data)-1,5])
-# close_rela = close_a - close_b 
-# open_rela  = np.array(data.iloc[1:,2]) - np.array(data.iloc[:len(data)-1,2])
-# high_rela = np.array(data.iloc[1:,3]) - np.array(data.iloc[:len(data)-1,3])
-# low_rela = np.array(data.iloc[1:,4]) - np.array(data.iloc[:len(data)-1,4])
+# data = pd.read_excel('data/dataset/XM_EURUSD-2013_H1.xlsx',header=None)
+# # close_diff = data.iloc[1:,5] - data.iloc[:len(data),5]
+# # close_a = np.array(data.iloc[1:,5])
+# # close_b = np.array(data.iloc[:len(data)-1,5])
+# # close_rela = close_a - close_b 
+# # open_rela  = np.array(data.iloc[1:,2]) - np.array(data.iloc[:len(data)-1,2])
+# # high_rela = np.array(data.iloc[1:,3]) - np.array(data.iloc[:len(data)-1,3])
+# # low_rela = np.array(data.iloc[1:,4]) - np.array(data.iloc[:len(data)-1,4])
 
-macd, macdsignal, macdhist = ta.MACD(data.iloc[:,5], fastperiod=12, slowperiod=26, signalperiod=9) ####
-ATR = ta.ATR(data.iloc[:,3], data.iloc[:,4], data.iloc[:,5], timeperiod=14)
-slowk, slowd = ta.STOCH(data.iloc[:,3], data.iloc[:,4], data.iloc[:,5], fastk_period=5, slowk_period=3, slowk_matype=0, slowd_period=3, slowd_matype=0)###
-WILL = ta.WILLR(data.iloc[:,3], data.iloc[:,4], data.iloc[:,5], timeperiod=14)
-SAR = ta.SAR(data.iloc[:,3], data.iloc[:,4], acceleration=0, maximum=0)
-aroondown, aroonup = ta.AROON(data.iloc[:,3], data.iloc[:,4], timeperiod=14)
+# macd, macdsignal, macdhist = ta.MACD(data.iloc[:,5], fastperiod=12, slowperiod=26, signalperiod=9) ####
+# ATR = ta.ATR(data.iloc[:,3], data.iloc[:,4], data.iloc[:,5], timeperiod=14)
+# slowk, slowd = ta.STOCH(data.iloc[:,3], data.iloc[:,4], data.iloc[:,5], fastk_period=5, slowk_period=3, slowk_matype=0, slowd_period=3, slowd_matype=0)###
+# WILL = ta.WILLR(data.iloc[:,3], data.iloc[:,4], data.iloc[:,5], timeperiod=14)
+# SAR = ta.SAR(data.iloc[:,3], data.iloc[:,4], acceleration=0, maximum=0)
+# aroondown, aroonup = ta.AROON(data.iloc[:,3], data.iloc[:,4], timeperiod=14)
 
 
-###################### 
+# ###################### 
  
-macd1, macdsignal1, macdhist1 = ta.MACD(data.iloc[:35,5], fastperiod=12, slowperiod=26, signalperiod=9)
-ATR1 = ta.ATR(data.iloc[:35,3], data.iloc[:35,4], data.iloc[:35,5], timeperiod=14)
-slowk1, slowd1 = ta.STOCH(data.iloc[:35,3], data.iloc[:35,4], data.iloc[:35,5], fastk_period=5, slowk_period=3, slowk_matype=0, slowd_period=3, slowd_matype=0)###
-WILL1 = ta.WILLR(data.iloc[:35,3], data.iloc[:35,4], data.iloc[:35,5], timeperiod=14)
-SAR1 = ta.SAR(data.iloc[:35,3], data.iloc[:35,4], acceleration=0, maximum=0)
-aroondown1, aroonup1 = ta.AROON(data.iloc[:35,3], data.iloc[:35,4], timeperiod=14)
+# macd1, macdsignal1, macdhist1 = ta.MACD(data.iloc[:35,5], fastperiod=12, slowperiod=26, signalperiod=9)
+# ATR1 = ta.ATR(data.iloc[:35,3], data.iloc[:35,4], data.iloc[:35,5], timeperiod=14)
+# slowk1, slowd1 = ta.STOCH(data.iloc[:35,3], data.iloc[:35,4], data.iloc[:35,5], fastk_period=5, slowk_period=3, slowk_matype=0, slowd_period=3, slowd_matype=0)###
+# WILL1 = ta.WILLR(data.iloc[:35,3], data.iloc[:35,4], data.iloc[:35,5], timeperiod=14)
+# SAR1 = ta.SAR(data.iloc[:35,3], data.iloc[:35,4], acceleration=0, maximum=0)
+# aroondown1, aroonup1 = ta.AROON(data.iloc[:35,3], data.iloc[:35,4], timeperiod=14)
 
-###################### 
+# ###################### 
  
-macd2, macdsignal2, macdhist2 = ta.MACD(data.iloc[:36,5], fastperiod=12, slowperiod=26, signalperiod=9)
-ATR2 = ta.ATR(data.iloc[:36,3], data.iloc[:36,4], data.iloc[:36,5], timeperiod=14)
-slowk2, slowd2 = ta.STOCH(data.iloc[:36,3], data.iloc[:36,4], data.iloc[:36,5], fastk_period=5, slowk_period=3, slowk_matype=0, slowd_period=3, slowd_matype=0)###
-WILL2 = ta.WILLR(data.iloc[:36,3], data.iloc[:36,4], data.iloc[:36,5], timeperiod=14)
-SAR2 = ta.SAR(data.iloc[:36,3], data.iloc[:36,4], acceleration=0, maximum=0)
-aroondown2, aroonup2 = ta.AROON(data.iloc[:36,3], data.iloc[:36,4], timeperiod=14)
-# macd1, macdsignal1, macdhist = ta.MACD(data.iloc[:,5], fastperiod=24, slowperiod=26, signalperiod=9)
-# macd2, macdsignal2, macdhist = ta.MACD(data.iloc[:,5], fastperiod=48, slowperiod=26, signalperiod=9)
-# macd3, macdsignal3, macdhist = ta.MACD(data.iloc[:,5], fastperiod=12, slowperiod=26, signalperiod=18)
-print("wait")
+# macd2, macdsignal2, macdhist2 = ta.MACD(data.iloc[:36,5], fastperiod=12, slowperiod=26, signalperiod=9)
+# ATR2 = ta.ATR(data.iloc[:36,3], data.iloc[:36,4], data.iloc[:36,5], timeperiod=14)
+# slowk2, slowd2 = ta.STOCH(data.iloc[:36,3], data.iloc[:36,4], data.iloc[:36,5], fastk_period=5, slowk_period=3, slowk_matype=0, slowd_period=3, slowd_matype=0)###
+# WILL2 = ta.WILLR(data.iloc[:36,3], data.iloc[:36,4], data.iloc[:36,5], timeperiod=14)
+# SAR2 = ta.SAR(data.iloc[:36,3], data.iloc[:36,4], acceleration=0, maximum=0)
+# aroondown2, aroonup2 = ta.AROON(data.iloc[:36,3], data.iloc[:36,4], timeperiod=14)
+# # macd1, macdsignal1, macdhist = ta.MACD(data.iloc[:,5], fastperiod=24, slowperiod=26, signalperiod=9)
+# # macd2, macdsignal2, macdhist = ta.MACD(data.iloc[:,5], fastperiod=48, slowperiod=26, signalperiod=9)
+# # macd3, macdsignal3, macdhist = ta.MACD(data.iloc[:,5], fastperiod=12, slowperiod=26, signalperiod=18)
+# print("wait")
 
 # fig = make_subplots(rows=4, cols=1,
 #                     shared_xaxes=True,
@@ -138,36 +138,38 @@ print("wait")
 # print(len(data1))
 # c_data = pd.concat([data,data1])
 # print(len(c_data))
-# for x in range(2011,2021):
+# data = []
+# for x in range(2010,2021):
 #     res = pd.read_excel('data/dataset/XM_EURUSD-'+ str(x) +'_H1.xlsx',header=None)
-#     data = pd.concat([data,res])
-# data.columns = ['date','time','open','high','low','close','volume']
-# macd, macdsignal, macdhist = ta.MACD(data['close'], fastperiod=12, slowperiod=26, signalperiod=9)
-# ATR = ta.ATR(data['high'], data['low'], data['close'], timeperiod=14)
-# slowk, slowd = ta.STOCH(data['high'], data['low'], data['close'], fastk_period=5, slowk_period=3, slowk_matype=0, slowd_period=3, slowd_matype=0)
-# WILL = ta.WILLR(data['high'], data['low'], data['close'], timeperiod=14)
-# SAR = ta.SAR(data['high'], data['low'], acceleration=0, maximum=0)
-# aroondown, aroonup = ta.AROON(data['high'], data['low'], timeperiod=14)
-# data = {
-#     'date' : data['date'],
-#     'time' : data['time'],
-#     'open' : data['open'],
-#     'high' : data['high'],
-#     'low'  : data['low'],
-#     'close' : data['close'],
-#     'volume' : data['volume'],
-#     'macd' : macd,
-#     'macdsignal':macdsignal,
-#     'macdhist':  macdhist, 
-#     'ATR' : ATR , 
-#     'slowk' : slowk, 
-#     'slowd' : slowd, 
-#     'WILL' : WILL,
-#     'SAR' : SAR,
-#     'aroondown' : aroondown,
-#     'aroonup' : aroonup
-#     }
-# all_data = pd.DataFrame(data= data)
+#     # data = pd.concat([data,res])
+#     res.columns = ['date','time','open','high','low','close','volume']
+#     macd, macdsignal, macdhist = ta.MACD(res['close'], fastperiod=12, slowperiod=26, signalperiod=9)
+#     ATR = ta.ATR(res['high'], res['low'], res['close'], timeperiod=14)
+#     slowk, slowd = ta.STOCH(res['high'], res['low'], res['close'], fastk_period=5, slowk_period=3, slowk_matype=0, slowd_period=3, slowd_matype=0)
+#     WILL = ta.WILLR(res['high'], res['low'], res['close'], timeperiod=14)
+#     SAR = ta.SAR(res['high'], res['low'], acceleration=0, maximum=0)
+#     aroondown, aroonup = ta.AROON(res['high'], res['low'], timeperiod=14)
+#     data = {
+#         'date' : res['date'],
+#         'time' : res['time'],
+#         'open' : res['open'],
+#         'high' : res['high'],
+#         'low'  : res['low'],
+#         'close' : res['close'],
+#         'volume' : res['volume'],
+#         'macd' : macd,
+#         'macdsignal':macdsignal,
+#         'macdhist':  macdhist, 
+#         'ATR' : ATR , 
+#         'slowk' : slowk, 
+#         'slowd' : slowd, 
+#         'WILL' : WILL,
+#         'SAR' : SAR,
+#         'aroondown' : aroondown,
+#         'aroonup' : aroonup
+#         }
+#     res_data = pd.DataFrame(data= data)
+#     data.append(res_data)
 # # print(all_data)
 # print(all_data.describe())
 # res = all_data.describe()
@@ -225,3 +227,20 @@ print("wait")
 
 
 
+all_data = []
+for x in range(2010,2021):
+    res = pd.read_excel('data\dataset_indy\XM_EURUSD-'+str(x)+'_H1_indy.xlsx')
+    res = res.iloc[:,2:]
+    res = res.to_numpy()
+    all_data.extend(res)
+# all_data.pop(0)
+all_data = pd.DataFrame(all_data)
+print(all_data.describe())
+res = all_data.describe()
+res.to_excel('test/describe.xlsx')
+# res = res.dropna()
+res1 = all_data.to_numpy()
+scaler = MinMaxScaler(feature_range=(-0.85,0.85))
+print(scaler.fit(res1))
+print(scaler.data_max_)
+pickle.dump(scaler,open('model/scaler.pickle', 'wb'))
